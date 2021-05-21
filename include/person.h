8faf0d8751e4b3034e111e11b43acd41087ea24f
Ishan Khandelwal <ishank3@illinois.edu>
2021-05-21 21:47:59 +0530

    Added Getters and constructor

--- include/person.h
+++ include/person.h
@@ -2,15 +2,38 @@
 // Created by khand on 5/20/2021.
 //
 
-#ifndef OBSQURAA
-#define OBSQURAA
+#ifndef OBSQURAA_PERSON_H
+#define OBSQURAA_PERSON_H
+
 #include <string>
 
 namespace obsquraa {
     class Person {
     public:
-        Person();
+        Person(std::size_t set_id, std::string set_name, std::string mail, std::string birth, std::string set_gender,
+               std::string set_height, std::string set_sexuality, std::string agepref, std::string place, std::string location,
+               bool same_place, std::string set_job, std::string fake_name, std::string set_tv_shows, std::string set_food,
+               std::string destination, std::string preference, std::string others);
+        std::size_t getID() const;
+        std::string getName() const;
+        std::string getEmail() const;
+        std::string getDateOfBirth() const;
+        std::string getGender() const;
+        std::string getHeight() const;
+        std::string getSexuality() const;
+        std::string getAgePref() const;
+        std::string getCity() const;
+        std::string getLocationPref() const;
+        bool getIfSameCityPref() const;
+        std::string getJob() const;
+        std::string getFakeNamePref() const;
+        std::string getTVShows() const;
+        std::string getFood() const;
+        std::string getDreamDestination() const;
+        std::string getPreferences() const;
+        std::string getExtra() const;
     private:
+        std::size_t id;
         std::string name;
         std::string email;
         std::string DOB;
@@ -32,4 +55,4 @@ namespace obsquraa {
 }
 
 
-#endif //OBSQURAA
+#endif //OBSQURAA_PERSON_H
--- src/person.cpp
+++ src/person.cpp
@@ -1,1 +1,100 @@
 #include <person.h>
+
+#include <utility>
+namespace obsquraa {
+
+    Person::Person(std::size_t set_id, std::string set_name, std::string mail, std::string birth, std::string set_gender,
+                   std::string set_height, std::string set_sexuality, std::string agepref, std::string place,
+                   std::string location, bool same_place, std::string set_job, std::string fake_name, std::string set_tv_shows,
+                   std::string set_food, std::string destination, std::string preference, std::string others) {
+        id = set_id;
+        name = std::move(set_name);
+        email = std::move(mail);
+        DOB = std::move(birth);
+        gender = std::move(set_gender);
+        height = std::move(set_height);
+        sexuality = std::move(set_sexuality);
+        age_preference = std::move(agepref);
+        city = std::move(place);
+        location_preference = std::move(location);
+        job = std::move(set_job);
+        fake_name_preference = std::move(fake_name);
+        tv_shows = std::move(set_tv_shows);
+        food = std::move(set_food);
+        dream_destination = std::move(destination);
+        preferences = std::move(preference);
+        extra = std::move(others);
+    }
+
+    std::size_t Person::getID() const {
+        return id;
+    }
+
+    std::string Person::getName() const {
+        return name;
+    }
+
+    std::string Person::getEmail() const {
+        return email;
+    }
+
+    std::string Person::getDateOfBirth() const {
+        return DOB;
+    }
+
+    std::string Person::getGender() const {
+        return gender;
+    }
+
+    std::string Person::getHeight() const {
+        return height;
+    }
+
+    std::string Person::getSexuality() const {
+        return sexuality;
+    }
+
+    std::string Person::getAgePref() const {
+        return age_preference;
+    }
+
+    std::string Person::getCity() const {
+        return city;
+    }
+
+    std::string Person::getLocationPref() const {
+        return location_preference;
+    }
+    
+    bool Person::getIfSameCityPref() const {
+        return same_city;
+    }
+
+    std::string Person::getJob() const {
+        return job;
+    }
+
+    std::string Person::getFakeNamePref() const {
+        return fake_name_preference;
+    }
+
+    std::string Person::getTVShows() const {
+        return tv_shows;
+    }
+
+    std::string Person::getFood() const {
+        return food;
+    }
+
+    std::string Person::getDreamDestination() const {
+        return dream_destination;
+    }
+
+    std::string Person::getPreferences() const {
+        return preferences;
+    }
+
+    std::string Person::getExtra() const {
+        return extra;
+    }
+}
