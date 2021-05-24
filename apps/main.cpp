7a403fc584936a98cbfb03427e0e8ffd49eba9bd
Ishan Khandelwal <ishank3@illinois.edu>
2021-05-24 17:17:46 +0530

    Tests json

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,3 +1,14 @@
+#include <iostream>
+#include "data_model.h"
+using obsquraa::DataModel;
 int main() {
+    std::string file = R"(C:\Users\khand\AppData\Roaming\Cinder\my-projects\obsquraa\data\Basic Info(1-14).json)";
+    std::ifstream new_file(file);
+    DataModel dataModel = DataModel();
+    dataModel.SetJson(file);
     return 0;
+//    nlohmann::json json = nlohmann::json::parse(new_file);
+//    for (auto &el : json.items()) {
+//        std::cout << el.value()["Gender"] << std::endl;
+//    }
 }
